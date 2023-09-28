86b0daa3a5d60dc30c60f68bee2378adc59bc243
Umair Rao <umairmuqeem@gmail.com>
2023-09-28 13:50:19 +0500

    Enable Theme Toggle button in Navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import Footer from "./components/footer/Footer";
 import Navbar from "./components/navbar/Navbar";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import ThemeProvider from "@/providers/ThemeProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <ThemeContextProvider>
+          <ThemeProvider>
         <div className="container">
           <div className="wrapper">
             <Navbar />
@@ -23,6 +25,7 @@ export default function RootLayout({ children }) {
             <Footer />
           </div>
         </div>
+        </ThemeProvider>
         </ThemeContextProvider>
       </body>
     </html>
