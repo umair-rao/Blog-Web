b43da12e15e28b46563abc9cb88cc4563c24b773
Umair Rao <umairmuqeem@gmail.com>
2023-09-28 12:53:07 +0500

    Create Components

---
+++ src/app/components/cardList.jsx/CardList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './cardList.module.css'
+
+const CardList = () => {
+  return (
+    <div className={styles.container}>
+      Navbar
+    </div>
+  )
+}
+
+export default CardList
