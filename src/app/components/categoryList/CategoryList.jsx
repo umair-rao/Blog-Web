a8dac60f510f906fd676459031e7ebea754c699d
Umair Rao <umairmuqeem@gmail.com>
2023-09-29 22:14:38 +0500

    Remove Extra code

--- src/app/components/categoryList/CategoryList.jsx
+++ src/app/components/categoryList/CategoryList.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import styles from './categoryList.module.css'
+import Link from 'next/link'
 
 const CategoryList = () => {
   return (
     <div className={styles.container}>
-      Navbar
+      <h1 className={styles.title}>Popular Categories</h1>
+      <div className={styles.categories}>
+        <div className={styles.category}>
+          <Link href='/blog?cat=style'>
+          </Link>
+        </div>
+        </div>
+      </div>
     </div>
   )
 }
