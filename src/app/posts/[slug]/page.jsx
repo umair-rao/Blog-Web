48e34002daa206c22c5236a222c9d2100132ddaa
Umair Rao <umairmuqeem@gmail.com>
2023-09-30 15:05:20 +0500

    Create posts page

---
+++ src/app/posts/[slug]/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './singlePage.module.css'
+
+const page = () => {
+  return (
+    <div>
+      <h1>Psots</h1>
+    </div>
+  )
+}
+
+export default page
