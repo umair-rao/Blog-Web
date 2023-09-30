c80bf70adaca80cfccf4748397ed3cba635413cd
Umair Rao <umairmuqeem@gmail.com>
2023-09-30 14:58:35 +0500

    Add Blog page

---
+++ src/app/blog/page.jsx
@@ -0,0 +1,20 @@
+import Menu from "../components/Menu/Menu";
+import CardList from "../components/cardList.jsx/CardList";
+import styles from "./blogPage.module.css";
+
+const BlogPage = ({ searchParams }) => {
+  const page = parseInt(searchParams.page) || 1;
+  const { cat } = searchParams;
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>{cat} Blog</h1>
+      <div className={styles.content}>
+        <CardList page={page} cat={cat}/>
+        <Menu />
+      </div>
+    </div>
+  );
+};
+
+export default BlogPage;
