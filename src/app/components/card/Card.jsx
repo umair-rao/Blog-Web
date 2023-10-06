2bdac892a749a4844614a2689e3489aa52a4b3f2
Umair Rao <umairmuqeem@gmail.com>
2023-10-06 11:35:16 +0500

    Add Next Prev Page

--- src/app/components/card/Card.jsx
+++ src/app/components/card/Card.jsx
@@ -2,25 +2,32 @@ import Image from 'next/image'
 import styles from './card.module.css'
 import Link from 'next/link'
 
-const Card = () => {
+const Card = ({key, item}) => {
   return (
-    <div className={styles.container}>
-      <div className={styles.imageContainer}>
-            <Image src='/p1.jpeg' alt='' fill className={styles.image} />
-          </div>
-          <div className={styles.textContainer}>
-            <div className={styles.detail}>
-                <span className={styles.date}>30.09.2023 - </span>
-                <span className={styles.category}>CULTURE</span>
-            </div>
-            <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo nam impedit temporibus rerum iure at veritatis numquam quos ab assumenda minima, earum fuga harum libero voluptate labore ipsa cumque aliquam.</h1>
-            <p className={styles.desc}>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut, deleniti. Deleniti quas adipisci quod repellat? Odio perferendis, porro sint consequatur aliquid magnam? Fugit, excepturi tenetur laboriosam ullam quo dignissimos praesentium.
-            </p>
-            <Link href='/' className={styles.link}>Read More</Link>
-          </div>
+    <div className={styles.container} key={key}>
+      {item.img && (
+        <div className={styles.imageContainer}>
+          <Image src={item.img} alt="" fill className={styles.image} />
+        </div>
+      )}
+      <div className={styles.textContainer}>
+        <div className={styles.detail}>
+          <span className={styles.date}>
+            {item.createdAt.substring(0, 10)} -{" "}
+          </span>
+          <span className={styles.category}>{item.catSlug}</span>
+        </div>
+        <Link href={`/posts/${item.slug}`}>
+          <h1>{item.title}</h1>
+        </Link>
+        {/* <p className={styles.desc}>{item.desc.substring(0, 60)}</p> */}
+        <div className={styles.desc} dangerouslySetInnerHTML={{ __html: item?.desc.substring(0,60) }}/>
+        <Link href={`/posts/${item.slug}`} className={styles.link}>
+          Read More
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Card
