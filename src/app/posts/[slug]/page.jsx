273cb639f0132e185214e3521d23ec2085ac0619
Umair Rao <umairmuqeem@gmail.com>
2023-10-06 16:35:34 +0500

    Remove open post page error

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -4,58 +4,58 @@ import Menu from "@/app/components/Menu/Menu";
 import Comments from "@/app/components/comments/Comments";
 
 const getData = async (slug) => {
-  const res = await fetch(`http://localhost:3000/api/posts/${slug}`, {
-    cache: "no-store",
-  });
-
-  if (!res.ok) {
-    throw new Error("Failed");
-  }
-
-  return res.json();
-};
-
-const SinglePage = async ({ params }) => {
-  const { slug } = params;
-
-  const data = await getData(slug);
+    const res = await fetch(`http://localhost:3000/api/posts/${slug}`, {
+      cache: "no-store",
+    });
+  
+    if (!res.ok) {
+      throw new Error("Failed");
+    }
+  
+    return res.json();
+  };
+  
+  const SinglePage = async ({ params }) => {
+    const { slug } = params;
+  
+    const data = await getData(slug);
 
   return (
     <div className={styles.container}>
-      <div className={styles.infoContainer}>
-        <div className={styles.textContainer}>
-          <h1 className={styles.title}>{data?.title}</h1>
-          <div className={styles.user}>
-            {data?.user?.image && (
-              <div className={styles.userImageContainer}>
-                <Image src={data.user.image} alt="" fill className={styles.avatar} />
-              </div>
-            )}
-            <div className={styles.userTextContainer}>
-              <span className={styles.username}>{data?.user.name}</span>
-              <span className={styles.date}>01.01.2024</span>
+    <div className={styles.infoContainer}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>{data?.title}</h1>
+        <div className={styles.user}>
+          {data?.user?.image && (
+            <div className={styles.userImageContainer}>
+              <Image src={data.user.image} alt="" fill className={styles.avatar} />
             </div>
+          )}
+          <div className={styles.userTextContainer}>
+            <span className={styles.username}>{data?.user.name}</span>
+            <span className={styles.date}>01.01.2024</span>
           </div>
         </div>
-        {data?.img && (
-          <div className={styles.imageContainer}>
-            <Image src={data.img} alt="" fill className={styles.image} />
-          </div>
-        )}
       </div>
-      <div className={styles.content}>
-        <div className={styles.post}>
-          <div
-            className={styles.description}
-            dangerouslySetInnerHTML={{ __html: data?.desc }}
-          />
-          <div className={styles.comment}>
-            <Comments postSlug={slug}/>
-          </div>
+      {data?.img && (
+        <div className={styles.imageContainer}>
+          <Image src={data.img} alt="" fill className={styles.image} />
         </div>
-        <Menu />
-      </div>
+      )}
     </div>
+    <div className={styles.content}>
+      <div className={styles.post}>
+        <div
+          className={styles.description}
+          dangerouslySetInnerHTML={{ __html: data?.desc }}
+        />
+        <div className={styles.comment}>
+          <Comments postSlug={slug}/>
+        </div>
+      </div>
+      <Menu />
+    </div>
+  </div>
   );
 };
 
